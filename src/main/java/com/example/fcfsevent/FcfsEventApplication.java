ca4313d5f72dc0c2466947105c017490923e9fd9
jeongho <qkrjh0904@gmail.com>
2023-08-10 23:54:57 +0900

    init

---
+++ src/main/java/com/example/fcfsevent/FcfsEventApplication.java
@@ -0,0 +1,13 @@
+package com.example.fcfsevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcfsEventApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcfsEventApplication.class, args);
+    }
+
+}

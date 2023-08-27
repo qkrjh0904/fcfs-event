ebafabbddddc41643cbc309faba2c8ba22e7ab5a
jeongho <qkrjh0904@gmail.com>
2023-08-27 22:56:18 +0900

    feat: 동시에 1000명을 발급시켜 동시성 이슈 테스트

--- api/src/test/java/com/example/api/service/CouponServiceTest.java
+++ api/src/test/java/com/example/api/service/CouponServiceTest.java
@@ -1,11 +1,14 @@
 package com.example.api.service;
 
 import com.example.api.repository.CouponRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -29,6 +32,32 @@ class CouponServiceTest {
 
         // then
         assertThat(count).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("동시에 쿠폰 발급")
+    public void concurrencyIssueACoupon() throws InterruptedException {
+        // given
+        int threadCount = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; ++i) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    couponService.issueACoupon(userId);
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+
+        countDownLatch.await();
+
+        long count = couponRepository.count();
+
+        assertThat(count).isEqualTo(100);
     }
 
 }
